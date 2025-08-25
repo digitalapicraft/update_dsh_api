b8c2451cbe496d306c3b165431be554161c485a1
AchinthyaU <achinthya.u@digitalapi.ai>
2025-08-25 18:51:23 +0530

    Update dsh_upload.sh

--- dsh_upload.sh
+++ dsh_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # CONFIG_FILE="dsh-config.json"
-API_URL="https://https://$INPUT_URL/api/v1/digital-content/documentation"
+API_URL="https://https://${{ inputs.url }}/api/v1/digital-content/documentation"
 LOGIN_URL="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}"
 USERNAME="${USERNAME}"
 PASSWORD="${PASSWORD}"
